393cac309eb376690d2d1af2bf45e34be590e677
Serhii Vandenvin <sevandenvin@gmail.com>
2022-09-01 15:26:52 +0300

    fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,12 +5,26 @@ const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
 
 const STORAGE_KEY = 'fedback-form-state';
-const formData = {};
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+checkLocalStoradge();
+populateInputFromLocalStoradge();
 
 form.addEventListener('input', throttle(onInputChange, 500));
 form.addEventListener('submit', onFormSubmit);
 
-populateInputFromLocalStoradge();
+function checkLocalStoradge() {
+  if (localStorage.getItem(STORAGE_KEY) === null) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+
+  formData.email = JSON.parse(localStorage.getItem(STORAGE_KEY)).email;
+  formData.message = JSON.parse(localStorage.getItem(STORAGE_KEY)).message;
+}
 
 function onInputChange(event) {
   formData[event.target.name] = event.target.value;
@@ -37,11 +51,24 @@ function onFormSubmit(event) {
 
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+
+  formData.email = '';
+  formData.message = '';
 }
 
 function populateInputFromLocalStoradge() {
   const savedInfo = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (savedInfo) {
+
+  if (savedInfo.email === '' && savedInfo.message === '') {
+    input.value = '';
+    textarea.value = '';
+  } else if (savedInfo.email !== '' && savedInfo.message === '') {
+    input.value = savedInfo.email;
+    textarea.value = '';
+  } else if (savedInfo.email === '' && savedInfo.message !== '') {
+    input.value = '';
+    textarea.value = savedInfo.message;
+  } else {
     input.value = savedInfo.email;
     textarea.value = savedInfo.message;
   }
