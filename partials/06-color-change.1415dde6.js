4d7c6d36b3fb807e5fd00a344f39cc6be4348561
Vandenvin <sevandenvin@gmail.com>
2022-09-06 17:03:56 +0000

    Deploying to gh-pages from @ Vandenvin/js-mini-projects@cbb7b14852cd6fbed2eabdfdb3fe8aace48e49f6 🚀

--- partials/06-color-change.1415dde6.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector(".color"),e=document.querySelector(".change-color"),t=document.querySelector("body");e.addEventListener("click",(function(){let e=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.textContent=e,t.style.backgroundColor=e}));
-//# sourceMappingURL=06-color-change.1415dde6.js.map
